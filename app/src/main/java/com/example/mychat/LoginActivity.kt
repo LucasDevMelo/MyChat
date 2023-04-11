c3d62eb21e606396574b2c9fde374f8d45fe0b65
LucasDevMelo <lucaslopesdemelosilva@gmail.com>
2023-04-11 01:46:12 -0300

    Atualizacao 11/04

--- app/src/main/java/com/example/mychat/LoginActivity.kt
+++ app/src/main/java/com/example/mychat/LoginActivity.kt
@@ -1,11 +1,18 @@
 package com.example.mychat
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        findViewById<TextView>(R.id.txt_account).setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/mychat/RegisterActivity.kt
+++ app/src/main/java/com/example/mychat/RegisterActivity.kt
@@ -2,10 +2,39 @@ package com.example.mychat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        findViewById<Button>(R.id.btn_register).setOnClickListener {
+            createUser()
+        }
+    }
+    private fun createUser() {
+        val email = findViewById<EditText>(R.id.edit_email).text.toString() // 1
+        val password = findViewById<EditText>(R.id.edit_password).text.toString() // 1
+        if (email.isEmpty() || password.isEmpty()) { // 2
+            Toast.makeText(this,
+
+                "email e senha devem ser informados",
+                Toast.LENGTH_LONG).show()
+
+            return
+        }
+        FirebaseAuth.getInstance()
+            .createUserWithEmailAndPassword(email, password) // 3
+            .addOnCompleteListener { // 4
+                if (it.isSuccessful) { // 5
+                    Log.i("Teste", "UserID é ${it.result?.user?.uid}") // 6
+                }
+            }
+            .addOnFailureListener { // 7
+                Log.e("Teste", it.message, it) // 8
+            }
     }
 }
